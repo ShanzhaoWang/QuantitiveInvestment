c132da1a4b117aa57d07f03cd588405447dcbb87
Shan <30907972+ShanzhaoWang@users.noreply.github.com>
2018-07-30 12:46:18 +0800

    Update CAPM section

--- Scr/Chapter2/CAPM.R
+++ Scr/Chapter2/CAPM.R
@@ -1,9 +1,9 @@
 library(quantmod) # download data
+library(PerformanceAnalytics)
 quantmod::getSymbols(c("IBM", "GE", "^GSPC"), from = "2010-01-01") #stock data
 
 head(GSPC)
 tail(GSPC)
-
 quantmod::barChart(GSPC) # candlestick
 
 #rename columns
@@ -25,4 +25,12 @@ ret = function(x){
   return(ret_x)
 }
 
-apply(dat, MARGIN = 2, ret) 
+dat_ret <- apply(dat, MARGIN = 2, ret)
+row.names(dat_ret) <- c(row.names(dat)[-1])
+
+# define annualized rate of return 4%, non-risking 
+Rf <- 0.04/12
+results_1 <- table.AnnualizedReturns(dat_ret, Rf = Rf)
+# One thing we observe from the result is that SP500 has an annual rate of return 10%, so it's not easy that we can surpass it
+
+results_2 <- table.Stats(dat_ret) # overall view of summary stats 
