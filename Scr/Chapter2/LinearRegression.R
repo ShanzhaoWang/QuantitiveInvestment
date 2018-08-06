04701e790c8b1dbbfdadc3ab7d77a62d771a7433
Shan <30907972+ShanzhaoWang@users.noreply.github.com>
2018-08-07 02:39:58 +0800

    Update LinearRegression.R

--- Scr/Chapter2/LinearRegression.R
+++ Scr/Chapter2/LinearRegression.R
@@ -1,3 +1,32 @@
 # load time series library
 library(zoo)
 library(xts)
+dat <-read.csv("test_data - Sheet1.csv", stringsAsFactors = F)
+
+# change it to xts format
+df <- xts(dat[, -1], order.by = as.POSIXct(dat[,1]))
+
+x <- as.numeric(df[,1])
+y <- as.numeric(df[,2])
+
+plot(y~x+1) 
+
+# slope and intercept, residuals, etc.
+lm.ab <- lm(y~x+1)
+lm.ab$coefficients
+lm.ab$call
+lm.ab$residuals
+summary(lm.ab) # F-test, p.66
+
+# run together 
+plot(y ~ x+ 1)
+abline(lm.ab)
+
+# residuals 
+y.res <- residuals(lm.ab)
+
+shapiro.test(y.res)
+plot(y.res)
+
+# four plots p.68
+plot(lm.ab)
